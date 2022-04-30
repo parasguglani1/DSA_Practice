596baa5e01e75e98f7fc0ccaf147b310b0f53f2c
paras <parasguglani1@gmail.com>
2022-04-30 19:08:57 +0530

    reformat

--- Binary_Search_Trees/BST.cpp
+++ Binary_Search_Trees/BST.cpp
@@ -10,7 +10,7 @@ Successful search: O(log n) || Unsuccessful search: O(n) */
 /*
 Binary Search Tree with following utilities:
 1. Insertion - iterative as well as recursive implementations
-Insert takes O(n) time, search takes O(log n): n x log n -> O(n log n) 
+Insert takes O(n) time, search takes O(log n): n x log n -> O(n log n)
 2. Search - iterative as well as recursive implementations
 3. Deletion - recursive implementation, utilizes height(BinNode*), predecessor(BinNode*) and successor(BinNode*)
 
@@ -25,20 +25,21 @@ You can choose either one randomly or base the choice on the height of the subtr
 However, even using this strategy may result in making other modifications on the tree, in case
 the inorder successor/predecessor has a child or not.
 Time taken: depends on the height, O(log n);
-subsequent modifications also depend on the height: max. O(log n) 
+subsequent modifications also depend on the height: max. O(log n)
 
 4. Initialization - using an array with a BST in preOrder/postOrder
 */
 #include "Stacks.h" // for pre-order and post-order initializations
-#include "Queue.h" // for deleting the binary search tree object
+#include "Queue.h"  // for deleting the binary search tree object
+#include <bits/stdc++.h>
 using namespace std;
 
 class BinNode
 {
-    public:
+public:
     int data;
-    BinNode* left;
-    BinNode* right;
+    BinNode *left;
+    BinNode *right;
     void newNode(int num)
     {
         data = num;
@@ -48,74 +49,75 @@ class BinNode
 
 class BinarySearchTree
 {
-    BinNode* root;
-    void inOrder(BinNode*); //inorder traversal and node-wise display
-    BinNode* rInsert(BinNode*, int); //recursively inserts new node
-    BinNode* rSearch(BinNode*, int); //recursively looks for key
-    void createFromPre(int*, int); //create tree from preorder
-    void createFromPost(int*, int); //create tree from postorder
+    BinNode *root;
+    void inOrder(BinNode *);          // inorder traversal and node-wise display
+    BinNode *rInsert(BinNode *, int); // recursively inserts new node
+    BinNode *rSearch(BinNode *, int); // recursively looks for key
+    void createFromPre(int *, int);   // create tree from preorder
+    void createFromPost(int *, int);  // create tree from postorder
     /*Deletion criteria*/
-    int height(BinNode*);
-    int numNodes(BinNode*);
+    int height(BinNode *);
+    int numNodes(BinNode *);
     /*Previous and Next InOrder element*/
-    BinNode* predecessor(BinNode*);
-    BinNode* successor(BinNode*);
-    //Delete element
-    BinNode* deleteEle(BinNode*, int);
-    public:
+    BinNode *predecessor(BinNode *);
+    BinNode *successor(BinNode *);
+    // Delete element
+    BinNode *deleteEle(BinNode *, int);
+
+public:
     BinarySearchTree()
     {
         root = NULL;
     }
-    BinarySearchTree(int*, int, int); 
-    void insert(int); //inserts new node
-    void inOrder(); //helper
-    void rInsert(int); //helper
-    BinNode* search(int); //returns key address
-    BinNode* rSearch(int); //helper
-    void deleteEle(int); //helper
+    BinarySearchTree(int *, int, int);
+    void insert(int);      // inserts new node
+    void inOrder();        // helper
+    void rInsert(int);     // helper
+    BinNode *search(int);  // returns key address
+    BinNode *rSearch(int); // helper
+    void deleteEle(int);   // helper
     ~BinarySearchTree();
 };
 
 void BinarySearchTree::insert(int toInsert)
 {
-    BinNode* ptr = root, *tail;
-    while(ptr)
+    BinNode *ptr = root, *tail;
+    while (ptr)
     {
         tail = ptr;
-        if(toInsert < ptr->data)
+        if (toInsert < ptr->data)
             ptr = ptr->left;
-        else if(toInsert > ptr->data)
+        else if (toInsert > ptr->data)
             ptr = ptr->right;
         else
-            return; //toInsert is already a part of the tree
+            return; // toInsert is already a part of the tree
     }
     ptr = new BinNode;
     ptr->newNode(toInsert);
-    if(!root)
+    if (!root)
         root = ptr;
     else
     {
-        if(ptr->data < tail->data)
+        if (ptr->data < tail->data)
             tail->left = ptr;
         else
-            tail->right = ptr; 
+            tail->right = ptr;
     }
 }
 
 void BinarySearchTree::inOrder()
 {
-    cout<<"Tree's inorder traversal: ";
+    cout << "Tree's inorder traversal: ";
     inOrder(root);
-    cout<<"\n";
+    cout << "\n";
 }
 
-void BinarySearchTree::inOrder(BinNode* ptr)
+void BinarySearchTree::inOrder(BinNode *ptr)
 {
-    if(ptr)
+    if (ptr)
     {
         inOrder(ptr->left);
-        cout<<ptr->data<<" ";
+        cout << ptr->data << " ";
         inOrder(ptr->right);
     }
 }
@@ -125,29 +127,29 @@ void BinarySearchTree::rInsert(int toInsert)
     root = rInsert(root, toInsert);
 }
 
-BinNode* BinarySearchTree::rInsert(BinNode* ptr, int toInsert)
+BinNode *BinarySearchTree::rInsert(BinNode *ptr, int toInsert)
 {
-    if(!ptr)
+    if (!ptr)
     {
         ptr = new BinNode;
         ptr->newNode(toInsert);
         return ptr;
     }
-    if(toInsert < ptr->data)
+    if (toInsert < ptr->data)
         ptr->left = rInsert(ptr->left, toInsert);
-    else if(toInsert > ptr->data)
+    else if (toInsert > ptr->data)
         ptr->right = rInsert(ptr->right, toInsert);
     return ptr;
 }
 
-BinNode* BinarySearchTree::search(int key)
+BinNode *BinarySearchTree::search(int key)
 {
-    BinNode* ptr = root;
-    while(ptr)
+    BinNode *ptr = root;
+    while (ptr)
     {
-        if(key == ptr->data)
+        if (key == ptr->data)
             break;
-        else if(key < ptr->data)
+        else if (key < ptr->data)
             ptr = ptr->left;
         else
             ptr = ptr->right;
@@ -155,91 +157,91 @@ BinNode* BinarySearchTree::search(int key)
     return ptr;
 }
 
-BinNode* BinarySearchTree::rSearch(int key)
+BinNode *BinarySearchTree::rSearch(int key)
 {
     return rSearch(root, key);
 }
 
-BinNode* BinarySearchTree::rSearch(BinNode* ptr, int key)
+BinNode *BinarySearchTree::rSearch(BinNode *ptr, int key)
 {
-    if(!ptr || ptr->data == key)
+    if (!ptr || ptr->data == key)
         return ptr;
-    else if(key < ptr->data)
+    else if (key < ptr->data)
         return rSearch(ptr->left, key);
     else
         return rSearch(ptr->right, key);
 }
 
-int BinarySearchTree::height(BinNode* ptr)
+int BinarySearchTree::height(BinNode *ptr)
 {
-    if(!ptr)
+    if (!ptr)
         return 0;
     int x = height(ptr->left);
     int y = height(ptr->right);
-    return (x > y)?x+1:y+1;
+    return (x > y) ? x + 1 : y + 1;
 }
 
-int BinarySearchTree::numNodes(BinNode* ptr)
+int BinarySearchTree::numNodes(BinNode *ptr)
 {
-    if(!ptr)
+    if (!ptr)
         return 0;
     return numNodes(ptr->left) + numNodes(ptr->right) + 1;
 }
 
-BinNode* BinarySearchTree::predecessor(BinNode* ptr)
+BinNode *BinarySearchTree::predecessor(BinNode *ptr)
 {
-    while(ptr && ptr->right)
+    while (ptr && ptr->right)
         ptr = ptr->right;
     return ptr;
 }
 
-BinNode* BinarySearchTree::successor(BinNode* ptr)
+BinNode *BinarySearchTree::successor(BinNode *ptr)
 {
-    while(ptr && ptr->left)
+    while (ptr && ptr->left)
         ptr = ptr->left;
     return ptr;
 }
 
-BinNode* BinarySearchTree::deleteEle(BinNode* ptr, int key)
+BinNode *BinarySearchTree::deleteEle(BinNode *ptr, int key)
 {
-    if(!ptr) //not found
+    if (!ptr) // not found
         return NULL;
     /*---physical deletion of a leaf node---*/
-    if(ptr && ptr->data == key && !ptr->left && !ptr->right) //first condition is redundant
+    if (ptr && ptr->data == key && !ptr->left && !ptr->right) // first condition is redundant
     {
-        if(!root->left && !root->right)
-            root = NULL; //if there's only root node
+        if (!root->left && !root->right)
+            root = NULL; // if there's only root node
         delete ptr;
         return NULL;
     }
     /*Deletion algorithm*/
-    //Search for the element to be deleted
-    if(key < ptr->data)
+    // Search for the element to be deleted
+    if (key < ptr->data)
         ptr->left = deleteEle(ptr->left, key);
-    else if(key > ptr->data)
+    else if (key > ptr->data)
         ptr->right = deleteEle(ptr->right, key);
     else
-    {//found
-        BinNode* q = NULL;
-        //Using height as deletion criteria
-        if(height(ptr->left) > height(ptr->right))
+    { // found
+        BinNode *q = NULL;
+        // Using height as deletion criteria
+        if (height(ptr->left) > height(ptr->right))
         {
-            /*finding element previous to the one 
+            /*finding element previous to the one
             to be deleted (in inOrder), in left subtree*/
             q = predecessor(ptr->left);
-            ptr->data = q->data; //deleted node effectively replaced
-            ptr->left = deleteEle(ptr->left, q->data); //go and delete the node from which we copied
+            ptr->data = q->data;                       // deleted node effectively replaced
+            ptr->left = deleteEle(ptr->left, q->data); // go and delete the node from which we copied
         }
         else
         {
-            /*finding element next to the one 
+            /*finding element next to the one
             to be deleted (in inOrder), in right subtree*/
             q = successor(ptr->right);
             ptr->data = q->data;
             ptr->right = deleteEle(ptr->right, q->data);
         }
     }
-    return ptr; //redundant, just like rInsert (had no explicit task)
+    return ptr; // redundant, just like rInsert (had no explicit task)
 }
 
 void BinarySearchTree::deleteEle(int key)
@@ -247,30 +249,30 @@ void BinarySearchTree::deleteEle(int key)
     root = deleteEle(root, key);
 }
 
-BinarySearchTree::BinarySearchTree(int* arr, int n, int flag):BinarySearchTree()
+BinarySearchTree::BinarySearchTree(int *arr, int n, int flag) : BinarySearchTree()
 {
-    if(flag == 0)
+    if (flag == 0)
         createFromPre(arr, n);
-    if(flag == 1)
+    if (flag == 1)
         createFromPost(arr, n);
     else
-        cout<<"Invalid argument passed\n";
+        cout << "Invalid argument passed\n";
 }
 
-void BinarySearchTree::createFromPre(int* preOrd, int n)
+void BinarySearchTree::createFromPre(int *preOrd, int n)
 {
-    if(root)
+    if (root)
         return;
     root = new BinNode;
     root->newNode(preOrd[0]);
-    
-    BinNode* newNode, *ptr = root; 
-    Stack<BinNode*> stk;
+
+    BinNode *newNode, *ptr = root;
+    Stack<BinNode *> stk;
     int i = 1, compWith;
-    
-    while(i < n)
+
+    while (i < n)
     {
-        if(preOrd[i] < ptr->data)
+        if (preOrd[i] < ptr->data)
         {
             newNode = new BinNode;
             newNode->newNode(preOrd[i++]);
@@ -278,36 +280,36 @@ void BinarySearchTree::createFromPre(int* preOrd, int n)
             stk.push(ptr);
             ptr = newNode;
         }
-        else if(preOrd[i] > ptr->data)
+        else if (preOrd[i] > ptr->data)
         {
-            compWith = (stk.isEmpty())?INT_MAX:stk.stackTop()->data;
-            if(preOrd[i] < compWith)
+            compWith = (stk.isEmpty()) ? INT_MAX : stk.stackTop()->data;
+            if (preOrd[i] < compWith)
             {
                 newNode = new BinNode;
                 newNode->newNode(preOrd[i++]);
                 ptr->right = newNode;
                 ptr = newNode;
             }
-            else if(preOrd[i] > compWith && !stk.isEmpty())
+            else if (preOrd[i] > compWith && !stk.isEmpty())
                 ptr = stk.pop();
         }
     }
 }
 
-void BinarySearchTree::createFromPost(int* postOrd, int n)
+void BinarySearchTree::createFromPost(int *postOrd, int n)
 {
-    if(root)
+    if (root)
         return;
     root = new BinNode;
-    root->newNode(postOrd[n-1]);
+    root->newNode(postOrd[n - 1]);
 
     BinNode *newNode, *ptr = root;
-    Stack<BinNode*> stk; 
-    int i = n-2, compWith;
+    Stack<BinNode *> stk;
+    int i = n - 2, compWith;
 
-    while(i >= 0)
+    while (i >= 0)
     {
-        if(postOrd[i] > ptr->data)
+        if (postOrd[i] > ptr->data)
         {
             newNode = new BinNode;
             newNode->newNode(postOrd[i--]);
@@ -315,17 +317,17 @@ void BinarySearchTree::createFromPost(int* postOrd, int n)
             stk.push(ptr);
             ptr = newNode;
         }
-        else if(postOrd[i] < ptr->data)
+        else if (postOrd[i] < ptr->data)
         {
-            compWith = (stk.isEmpty())?INT_MIN:stk.stackTop()->data;
-            if(postOrd[i] > compWith)
+            compWith = (stk.isEmpty()) ? INT_MIN : stk.stackTop()->data;
+            if (postOrd[i] > compWith)
             {
                 newNode = new BinNode;
                 newNode->newNode(postOrd[i--]);
                 ptr->left = newNode;
                 ptr = newNode;
             }
-            else if(postOrd[i] < compWith && !stk.isEmpty())
+            else if (postOrd[i] < compWith && !stk.isEmpty())
                 ptr = stk.pop();
         }
     }
@@ -333,19 +335,20 @@ void BinarySearchTree::createFromPost(int* postOrd, int n)
 
 BinarySearchTree::~BinarySearchTree()
 {
-    Queue<BinNode*> que;
-    que.enqueue(root); BinNode* temp;
-    while(!que.isEmpty())
+    Queue<BinNode *> que;
+    que.enqueue(root);
+    BinNode *temp;
+    while (!que.isEmpty())
     {
         temp = que.dequeue();
-        if(temp->left)
+        if (temp->left)
             que.enqueue(temp->left);
-        if(temp->right)
+        if (temp->right)
             que.enqueue(temp->right);
         delete temp;
     }
     root = NULL;
-    cout<<"Binary Search Tree destructed\n";
+    cout << "Binary Search Tree destructed\n";
 }
 
 int main()
@@ -360,9 +363,9 @@ int main()
     b.insert(4);
     b.rInsert(37);
     b.deleteEle(30);
-    b.deleteEle(0); //element is not in tree
-    cout<<"30 found at address: "<<b.rSearch(30)<<"\n";
-    cout<<"20 found at address: "<<b.search(20)<<"\n";
+    b.deleteEle(0); // element is not in tree
+    cout << "30 found at address: " << b.rSearch(30) << "\n";
+    cout << "20 found at address: " << b.search(20) << "\n";
     b.inOrder();
     return 0;
 }
--- array representations/4-2d-array.cpp
+++ array representations/4-2d-array.cpp
@@ -2,10 +2,20 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
 
     // Declaring and initializing a 2D array (3x4) inside the stack
     int A[3][4] = {{1, 2, 3, 4}, {2, 4, 6, 8}, {3, 5, 7, 9}};
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         cout << "A [" << i << "] [" << j << "] is " << A[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     // Creating a 2D array (3x4) with pointers
     int *P[3]; // Declaring an array of pointers (inside the stack)
@@ -20,17 +30,19 @@ int main(void) {
     // Another method, using double pointers to create a 3x4 array
     int **Q; // Created inside stack, will point to an array in heap
 
-    Q = new int*[3]; // Now an array of pointers is created in heap and Q points at it
+    Q = new int *[3]; // Now an array of pointers is created in heap and Q points at it
 
     Q[0] = new int[4]; // Assigning rows to the array of pointers
     Q[1] = new int[4];
     Q[2] = new int[4];
     // Now the entire array is inside heap, only the double pointer is inside the stack
 
-    int counter = 1;
+    int counter = 10;
     // It's possible to access all of these arrays like this:
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
             Q[i][j] = counter++; // Initializing them from 1 to 12
             cout << Q[i][j] << " "; // Printing rows x columns
         }
@@ -40,18 +52,18 @@ int main(void) {
     // In C:
     int *B[3]; // Rows created inside the stack
 
-    B[0] = (int *)malloc(4*sizeof(int)); // Columns created inside heap
-    B[1] = (int *)malloc(4*sizeof(int));
-    B[2] = (int *)malloc(4*sizeof(int));
+    B[0] = (int *)malloc(4 * sizeof(int)); // Columns created inside heap
+    B[1] = (int *)malloc(4 * sizeof(int));
+    B[2] = (int *)malloc(4 * sizeof(int));
 
     // Array inside heap only:
     int **C; // Double pointer inside the stack
 
-    C = (int **)malloc(3*sizeof(int *)); // Rows inside heap
+    C = (int **)malloc(3 * sizeof(int *)); // Rows inside heap
 
-    C[0] = (int *)malloc(4*sizeof(int)); // Columns inside heap
-    C[1] = (int *)malloc(4*sizeof(int));
-    C[2] = (int *)malloc(4*sizeof(int));
+    C[0] = (int *)malloc(4 * sizeof(int)); // Columns inside heap
+    C[1] = (int *)malloc(4 * sizeof(int));
+    C[2] = (int *)malloc(4 * sizeof(int));
 
     return 0;
 }
