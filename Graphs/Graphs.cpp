1b313e16f457ef52dacb0e389c753a9e22e26e6e
paras <parasguglani1@gmail.com>
2023-01-05 14:42:57 +0530

    formatting

--- Graphs/Graphs.cpp
+++ Graphs/Graphs.cpp
@@ -55,69 +55,73 @@ Time taken: O(n)
 */
 
 // Performing BFS and DFS on graphs represented using adjacent matrices
-/* Displaying edges of the minimum-cost spanning tree using: 
+/* Displaying edges of the minimum-cost spanning tree using:
 1. Djikstra, Jarnik and Prim's algorithm
 2. Kruskal's algorithm*/
-# include <iostream>
-# include <queue>
+#include <iostream>
+#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 #define inf INT_MAX
 
-void del2D(int** ar, int x)
+void del2D(int **ar, int x)
 {
-    for(int i = 0; i < x; i++)
+    for (int i = 0; i < x; i++)
         delete[] ar[i];
     delete[] ar;
 }
 class Graph
 {
-    private:
-    int** adj;
+private:
+    int **adj;
     int n;
-    void fill(int*);
-    int* init(int, int);
+    void fill(int *);
+    int *init(int, int);
     int numEdges();
-    int** onlyEdges(int);
-    int find(int*, int);
-    void Union(int*, int, int);
+    int **onlyEdges(int);
+    int find(int *, int);
+    void Union(int *, int, int);
     void BreadthFirstSearch(int);
-    void DFS(int*, int);
-    public:
-    Graph(int*, int);
+    void DFS(int *, int);
+
+public:
+    Graph(int *, int);
     void BFS(int);
     void DFS(int);
-    int** MSTPrims();
-    int** MSTKruskal();
+    int **MSTPrims();
+    int **MSTKruskal();
     ~Graph();
 };
-void Graph::fill(int* arr)
+void Graph::fill(int *arr)
 {
-    for(int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for(int j = 1; j < n; j++)
-            adj[i][j] = *(arr+(i-1)*(n-1)+(j-1)) == 0 ? inf : *(arr+(i-1)*(n-1)+(j-1));
+        for (int j = 1; j < n; j++)
+            adj[i][j] = *(arr + (i - 1) * (n - 1) + (j - 1)) == 0 ? inf : *(arr + (i - 1) * (n - 1) + (j - 1));
     }
 }
-int* Graph::init(int toFill, int n)
+int *Graph::init(int toFill, int n)
 {
-    int* arr = new int[n];
-    for(int i = 0; i < n; i++)
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
         arr[i] = toFill;
     return arr;
 }
 void Graph::BreadthFirstSearch(int start)
 {
-    queue<int> que; int* visit = init(0, n);
+    queue<int> que;
+    int *visit = init(0, n);
     cout << start << " ";
     visit[start] = 1;
     que.push(start);
-    while(!que.empty())
+    while (!que.empty())
     {
-        start = que.front(); que.pop();
-        for(int i = 1; i < n; i++)
+        start = que.front();
+        que.pop();
+        for (int i = 1; i < n; i++)
         {
-            if(adj[start][i] != inf && visit[i] == 0)
+            if (adj[start][i] != inf && visit[i] == 0)
             {
                 cout << i << " ";
                 visit[i] = 1;
@@ -127,48 +131,51 @@ void Graph::BreadthFirstSearch(int start)
     }
     delete[] visit;
 }
-void Graph::DFS(int* visit, int start)
+void Graph::DFS(int *visit, int start)
 {
-    if(visit[start] == 0)
+    if (visit[start] == 0)
     {
         cout << start << " ";
         visit[start] = 1;
-        for(int i = 1; i < n; i++)
-            if(adj[start][i] != inf)
+        for (int i = 1; i < n; i++)
+            if (adj[start][i] != inf)
                 DFS(visit, i);
     }
 }
-Graph::Graph(int* arr, int x)
+Graph::Graph(int *arr, int x)
 {
     n = (x > 0) ? x + 1 : 6;
-    adj = new int*[n];
-    for(int i = 0; i < n; i++)
+    adj = new int *[n];
+    for (int i = 0; i < n; i++)
         adj[i] = new int[n];
     fill(arr);
 }
 void Graph::BFS(int start)
 {
     cout << "Graph in breadth-first search: ";
-    BreadthFirstSearch(start); cout << "\n";
+    BreadthFirstSearch(start);
+    cout << "\n";
 }
 void Graph::DFS(int start)
 {
-    int* visit = init(0, n);
+    int *visit = init(0, n);
     cout << "Graph in depth-first search: ";
-    DFS(visit, start); cout << "\n";
+    DFS(visit, start);
+    cout << "\n";
     delete[] visit;
 }
-int** Graph::MSTPrims()
+int **Graph::MSTPrims()
 {
-    int* near = init(inf, n), **result = new int*[2];
-    result[0] = new int[n-2]; result[1] = new int[n-2]; 
+    int *near = init(inf, n), **result = new int *[2];
+    result[0] = new int[n - 2];
+    result[1] = new int[n - 2];
     // one-time procedure
     int min = inf, u, v;
-    for(int i = 1; i < n; i++) // minimum in upper triangle
+    for (int i = 1; i < n; i++) // minimum in upper triangle
     {
-        for(int j = i; j < n; j++)
+        for (int j = i; j < n; j++)
         {
-            if(adj[i][j] < min)
+            if (adj[i][j] < min)
             {
                 min = adj[i][j];
                 u = i, v = j;
@@ -176,34 +183,34 @@ int** Graph::MSTPrims()
         }
     }
     result[0][0] = u, result[1][0] = v; // push the edge
-    near[u] = near[v] = 0; // zero out the indices
-    for(int i = 1; i < n; i++) // set the least costliest
+    near[u] = near[v] = 0;              // zero out the indices
+    for (int i = 1; i < n; i++)         // set the least costliest
     {
-        if(near[i] != 0)
+        if (near[i] != 0)
         {
-            if(adj[i][u] < adj[i][v])
+            if (adj[i][u] < adj[i][v])
                 near[i] = u;
             else
                 near[i] = v;
         }
     }
     // repeating procedure
-    for(int res = 1; res < n-2; res++)
+    for (int res = 1; res < n - 2; res++)
     {
         min = inf;
-        for(int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
-            if(near[i] != 0 && adj[i][near[i]] < min)
+            if (near[i] != 0 && adj[i][near[i]] < min)
             {
                 min = adj[i][near[i]];
                 u = i; // min cost index
             }
         }
         result[0][res] = u, result[1][res] = near[u]; // push the edge
-        near[u] = 0; // zero out the vertex's index
-        for(int i = 1; i < n; i++) // rejig the least costliest
+        near[u] = 0;                                  // zero out the vertex's index
+        for (int i = 1; i < n; i++)                   // rejig the least costliest
         {
-            if(near[i] != 0 && adj[i][u] < adj[i][near[i]])
+            if (near[i] != 0 && adj[i][u] < adj[i][near[i]])
                 near[i] = u;
         }
     }
@@ -212,11 +219,11 @@ int** Graph::MSTPrims()
 int Graph::numEdges()
 {
     int edgeSize = 0;
-    for(int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for(int j = i; j < n; j++)
+        for (int j = i; j < n; j++)
         {
-            if(adj[i][j] != inf)
+            if (adj[i][j] != inf)
             {
                 edgeSize++; // number of edges
             }
@@ -224,17 +231,17 @@ int Graph::numEdges()
     }
     return edgeSize;
 }
-int** Graph::onlyEdges(int edgeSize)
+int **Graph::onlyEdges(int edgeSize)
 {
-    int** edges = new int*[3]; // v1, v2, weight
-    for(int i = 0; i < 3; i++)
+    int **edges = new int *[3]; // v1, v2, weight
+    for (int i = 0; i < 3; i++)
         edges[i] = new int[edgeSize];
     int k = 0;
-    for(int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for(int j = i; j < n; j++)
+        for (int j = i; j < n; j++)
         {
-            if(adj[i][j] != inf)
+            if (adj[i][j] != inf)
             {
                 edges[0][k] = i;
                 edges[1][k] = j;
@@ -245,9 +252,9 @@ int** Graph::onlyEdges(int edgeSize)
     }
     return edges;
 }
-void Graph::Union(int* s, int u, int v)
+void Graph::Union(int *s, int u, int v)
 {
-    if(s[u] < s[v])
+    if (s[u] < s[v])
     {
         s[u] = s[u] + s[v];
         s[v] = u;
@@ -258,39 +265,39 @@ void Graph::Union(int* s, int u, int v)
         s[u] = v;
     }
 }
-int Graph::find(int* s, int u)
+int Graph::find(int *s, int u)
 {
-    while(s[u] > 0)
+    while (s[u] > 0)
     {
         u = s[u];
     }
     return u;
 }
-int** Graph::MSTKruskal()
+int **Graph::MSTKruskal()
 {
     int numEdge = numEdges();
-    int** edgeWeights = onlyEdges(numEdge); // 2D matrix with dimensions 3*(numEdges)
-    int* included = init(0, numEdge); // denotes which of the edges have been accounted for
-    int* disjoint = init(-1, n); // prevents formation of cycles
-    int** result = new int*[2]; // to return, the answer
-    for(int i = 0; i < 2; i++)
-        result[i] = new int[n-2];
+    int **edgeWeights = onlyEdges(numEdge); // 2D matrix with dimensions 3*(numEdges)
+    int *included = init(0, numEdge);       // denotes which of the edges have been accounted for
+    int *disjoint = init(-1, n);            // prevents formation of cycles
+    int **result = new int *[2];            // to return, the answer
+    for (int i = 0; i < 2; i++)
+        result[i] = new int[n - 2];
     // the iterative operation
     int i = 0, min, k, u, v, p1, p2;
-    while(i < n-2)
+    while (i < n - 2)
     {
         min = inf;
-        for(int j = 0; j < numEdge; j++)
+        for (int j = 0; j < numEdge; j++)
         {
-            if(!included[j] && edgeWeights[2][j] < min)
+            if (!included[j] && edgeWeights[2][j] < min)
             {
                 min = edgeWeights[2][j];
                 k = j; // index for minimum weight
             }
         }
         u = edgeWeights[0][k], v = edgeWeights[1][k];
-        p1 = find(disjoint, u), p2 = find(disjoint, v); 
-        if(p1 != p2) // if cycle is not formed (dissimilar parents)
+        p1 = find(disjoint, u), p2 = find(disjoint, v);
+        if (p1 != p2) // if cycle is not formed (dissimilar parents)
         {
             result[0][i] = u;
             result[1][i] = v;
@@ -326,20 +333,20 @@ int main()
                    {0, 11, 24, 0, 7, 5},
                    {0, 0, 0, 7, 0, 0},
                    {0, 0, 0, 5, 0, 0}};
-    Graph g((int*)s, n);
+    Graph g((int *)s, n);
     g.BFS(k);
     g.DFS(k);
-    int** mstDJP = g.MSTPrims(), **mstK = g.MSTKruskal();
+    int **mstDJP = g.MSTPrims(), **mstK = g.MSTKruskal();
     cout << "Minimum-cost spanning tree edges:\n";
     cout << "1. Using Djikstra, Jarnik and Prim's algorithm:\n\t";
-    for(int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
         cout << "(" << mstDJP[0][i] << ", " << mstDJP[1][i] << ")\t";
     cout << "\n";
     cout << "2. Using Kruskal's algorithm:\n\t";
-    for(int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
         cout << "(" << mstK[0][i] << ", " << mstK[1][i] << ")\t";
     cout << "\n";
-    del2D(mstDJP, 2); 
+    del2D(mstDJP, 2);
     del2D(mstK, 2);
     return 0;
 }
--- Matrices/DiagonalMatrix.c
+++ Matrices/DiagonalMatrix.c
@@ -1,32 +1,34 @@
-//Implementing a 2D diagonal matrix using a structure in C
+// Implementing a 2D diagonal matrix using a structure in C
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 struct DiagMat
 {
     int n;
-    int filled_element; //privy to the structure
-    int* arr;
+    int filled_element; // privy to the structure
+    int *arr;
 };
 
-void allocate(struct DiagMat* d)
+void allocate(struct DiagMat *d)
 {
     int x, y;
-    while(1)
+    while (1)
     {
         printf("Enter the (x,y) matrix dimensions: ");
         scanf("%d %d", &x, &y);
-        if(x == y && x > 0 && y > 0)
+        if (x == y && x > 0 && y > 0)
             break;
         printf("Invalid input, enter non-negative and equal dimensions\n");
     }
-    d->n = x; d->filled_element = 0;
-    d->arr = (int*)calloc(x, sizeof(int));
+    d->n = x;
+    d->filled_element = 0;
+    d->arr = (int *)calloc(x, sizeof(int));
 }
 
-void deallocate(struct DiagMat* d)
+void deallocate(struct DiagMat *d)
 {
-    if(d->n != 0)
+    if (d->n != 0)
     {
         free(d->arr);
         d->n = 0;
@@ -35,37 +37,37 @@ void deallocate(struct DiagMat* d)
     }
 }
 
-void set(struct DiagMat* d, int i, int j, int x)
+void set(struct DiagMat *d, int i, int j, int x)
 {
-    if(i == j && i > 0 && j > 0 && i <= d->n && j <= d->n)
+    if (i == j && i > 0 && j > 0 && i <= d->n && j <= d->n)
     {
-        d->arr[i-1] = x;
+        d->arr[i - 1] = x;
         printf("%dth column in the %dth row was set as %d\n", i, j, x);
         d->filled_element += 1;
     }
     else
     {
-        if(i != j && i > 0 && j > 0 && i <= d->n && j <= d->n)
+        if (i != j && i > 0 && j > 0 && i <= d->n && j <= d->n)
             printf("%dth column in the %dth row was set as 0\n", i, j);
-        else if(i < 0 || j < 0)
+        else if (i < 0 || j < 0)
             printf("Invalid index, please enter positive numbers only\n");
-        else if(i > d->n || j > d->n)
+        else if (i > d->n || j > d->n)
             printf("Invalid index, out of bounds\n");
     }
 }
 
 int get(struct DiagMat d, int i, int j)
 {
-    if(d.filled_element == 0)
+    if (d.filled_element == 0)
     {
         printf("Matrix is empty\n");
         return INT_MIN;
     }
-    if(i == j && i <= d.n && j <= d.n)
-        return d.arr[i-1];
+    if (i == j && i <= d.n && j <= d.n)
+        return d.arr[i - 1];
     else
     {
-        if(i > 0 && j > 0 && i <= d.n && j <= d.n)
+        if (i > 0 && j > 0 && i <= d.n && j <= d.n)
             return 0;
         else
         {
@@ -77,15 +79,15 @@ int get(struct DiagMat d, int i, int j)
 
 void display(struct DiagMat d)
 {
-    if(d.filled_element == 0)
+    if (d.filled_element == 0)
     {
         printf("Matrix is empty\n");
         return;
     }
     int i, j;
-    for(i = 1; i <= d.n; i++)
+    for (i = 1; i <= d.n; i++)
     {
-        for(j = 1; j <= d.n; j++)
+        for (j = 1; j <= d.n; j++)
             printf("%d ", get(d, i, j));
         printf("\n");
     }
@@ -94,45 +96,49 @@ void display(struct DiagMat d)
 int main()
 {
     struct DiagMat d;
-    allocate(&d); int choice;
+    allocate(&d);
+    int choice;
 
-    while(1)
+    while (1)
     {
         printf("1. Enter an element in the matrix\n2. Get an element from the matrix\n");
         printf("3. Display matrix contents\n4. Exit the menu\n");
-        printf("Enter your choice: "); scanf("%d", &choice);
-        switch(choice)
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
         {
-            case 1:
-            {
-                int x, y, element;
-                printf("Enter the ith and jth dimension in 1-based indexing: ");
-                scanf("%d %d", &x, &y);
-                printf("Enter the element: "); scanf("%d", &element);
-                set(&d, x, y, element);
-                break;                
-            }
-            case 2:
-            {
-                int x, y, element;
-                printf("Enter the ith and jth dimension in 1-based indexing: ");
-                scanf("%d %d", &x, &y); element = get(d, x, y);
-                if(x > 0 && y > 0 && x <= d.n && y <= d.n && d.filled_element != 0)
-                    printf("Element at %dth column in %dth row is: %d\n", x, y, element);
-                break;
-            }
-            case 3:
-            {
-                display(d);
-                break;
-            }
-            case 4:
-                printf("Menu exited\n");
-                break;
-            default:
-                printf("Invalid choice, try again\n");
+        case 1:
+        {
+            int x, y, element;
+            printf("Enter the ith and jth dimension in 1-based indexing: ");
+            scanf("%d %d", &x, &y);
+            printf("Enter the element: ");
+            scanf("%d", &element);
+            set(&d, x, y, element);
+            break;
         }
-        if(choice == 4)
+        case 2:
+        {
+            int x, y, element;
+            printf("Enter the ith and jth dimension in 1-based indexing: ");
+            scanf("%d %d", &x, &y);
+            element = get(d, x, y);
+            if (x > 0 && y > 0 && x <= d.n && y <= d.n && d.filled_element != 0)
+                printf("Element at %dth column in %dth row is: %d\n", x, y, element);
+            break;
+        }
+        case 3:
+        {
+            display(d);
+            break;
+        }
+        case 4:
+            printf("Menu exited\n");
+            break;
+        default:
+            printf("Invalid choice, try again\n");
+        }
+        if (choice == 4)
             break;
     }
     deallocate(&d);
